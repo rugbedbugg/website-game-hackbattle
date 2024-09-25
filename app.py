3d5eb4ae77043e9e81930f670513a89e200f5a3c
Partha <160270614+rugbedbugg@users.noreply.github.com>
2024-09-25 15:22:01 +0530

    corrected syntax error for web app

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ def home():
 
 @app.route('/<gamename>')
 def game1(gamename):
-    return render_template('game1.html' gamename = Flappy Bird)
+    return render_template('game1.html', gamename="Flappy Bird")
 
 @app.route('/<gamename>')
 def game2(gamename):
-    return render_template('game2.html' gamename = Snake Game)
+    return render_template('game2.html', gamename ="Snake Game")
 
 @app.route('/<gamename>')
 def game3(gamename):
-    return render_template('game3.html' gamename = Space Invaders)
+    return render_template('game3.html', gamename="Space Invaders")
 
 @app.route('/<gamename>')
 def game4(gamename):
-    return render_template('game4.html' gamename = no_name)
+    return render_template('game4.html', gamename ="no_name")
 
 if __name__ == '__main__':
     app.run(debug=True)
