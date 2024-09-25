3779d38c1e351bede888c29267707470c31ed731
aritraghosh2005 <aritraghosh.ag2005@gmail.com>
2024-09-25 13:07:48 +0530

    Setting black canvas

---
+++ Snake Game/src/SnakeGame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel
+{
+    int boardWidth;
+    int boardHeight;
+
+    SnakeGame(int boardWidth, int boardHeight)
+    {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.black);
+
+    }
+}
