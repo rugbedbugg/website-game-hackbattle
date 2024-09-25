c2edfd1053863ecda9a3db6521b9edb66d01166a
Partha <160270614+rugbedbugg@users.noreply.github.com>
2024-09-25 10:53:14 +0530

    Updated app.py
    1. Added proper jinja elements in the webpage design
    2. Working on adding a feature where upon hovering, game 
        description appears in a windows
    3. Website now displays the game webpage title according to the 
        game being played.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import flask as Flask, redirect, url_for
+import flask as Flask
+import render_template as render_template
+import redirect as redirect
+import url_for as url_for
 
 app = Flask(__name__)
 
@@ -7,24 +10,24 @@ def home_redir():
     return redirect(url_for('home.html'))
 
 @app.route('/home')
-def homepage():
+def home():
     return 'home.html'
 
 @app.route('/<gamename>')
-def game1():
-    return 'game1.html'
+def game1(gamename):
+    return render_template('game1.html' gamename = Flappy Bird)
 
 @app.route('/<gamename>')
-def game2():
-    return 'game2.html'
+def game2(gamename):
+    return render_template('game2.html' gamename = Snake Game)
 
 @app.route('/<gamename>')
-def game3():
-    return 'game3.html'
+def game3(gamename):
+    return render_template('game3.html' gamename = Space Invaders)
 
 @app.route('/<gamename>')
-def game4():
-    return 'game4.html'
+def game4(gamename):
+    return render_template('game4.html' gamename = no_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
