083dfe6b42241e3302323d2b7f00c99f4fd1d1c8
Partha <160270614+rugbedbugg@users.noreply.github.com>
2024-09-26 01:11:20 +0530

    removed a game

--- app.py
+++ app.py
@@ -4,31 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template(redirect(url_for("home.html")))
+    return render_template(redirect(url_for("home")))
 
 @app.route('/<gamename>')
 def pre_game1(gamename):
-    return render_template(redirect(url_for("pre_game1.html")), gamename="Aritra's Game")
+    return render_template(redirect(url_for("pre_game1")), gamename="Aritra's Game")
 
 @app.route('/<gamename>')
 def pre_game2(gamename):
-    return render_template(redirect(url_for("pre_game2.html")), gamename="Space Invaders")
+    return render_template(redirect(url_for("pre_game2")), gamename="Space Invaders")
 
 @app.route('/<gamename>')
 def pre_game3(gamename):
-    return render_template(redirect(url_for("pre_game3.html")), gamename="Flappy Bird")
+    return render_template(redirect(url_for("pre_game3")), gamename="Flappy Bird")
 
 @app.route('/<gamename>')
 def pre_game4(gamename):
-    return render_template(redirect(url_for("pre_game4.html")), gamename='Feed The "Python"')
-
-@app.route('/<gamename>')
-def pre_game5(gamename):
-    return render_template(redirect(url_for("pre_game5.html")), gamename="Memory Mayhem")
+    return render_template(redirect(url_for("pre_game4")), gamename='Feed The "Python"')
 
 @app.route('/<gamename>')
 def pre_game6(gamename):
-    return render_template(redirect(url_for("pre_game6.html")), gamename="Tetris")
+    return render_template(redirect(url_for("pre_game6")), gamename="Tetris")
 
 if __name__ == '__main__':
     app.run(debug=True)
