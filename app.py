a13f535807820a13b8ee3a5e967773869dc6292f
Partha <160270614+rugbedbugg@users.noreply.github.com>
2024-09-26 04:10:19 +0530

    Created stable home page

--- app.py
+++ app.py
@@ -4,27 +4,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template(redirect(url_for("home")))
+    return render_template('home.html')
 
 @app.route('/<gamename>')
 def pre_game1(gamename):
-    return render_template(redirect(url_for("pre_game1")), gamename="Aritra's Game")
+    return redirect(url_for("pre_game1"))
 
-@app.route('/<gamename>')
+@app.route('/Space Invaders')
 def pre_game2(gamename):
-    return render_template(redirect(url_for("pre_game2")), gamename="Space Invaders")
+    return redirect(url_for("pre_game2"))
 
-@app.route('/<gamename>')
+@app.route('/Flappy Bird')
 def pre_game3(gamename):
-    return render_template(redirect(url_for("pre_game3")), gamename="Flappy Bird")
+    return redirect(url_for("pre_game3"))
 
-@app.route('/<gamename>')
+@app.route('/Feed The "Python"')
 def pre_game4(gamename):
-    return render_template(redirect(url_for("pre_game4")), gamename='Feed The "Python"')
+    return redirect(url_for("pre_game4"))
 
-@app.route('/<gamename>')
-def pre_game6(gamename):
-    return render_template(redirect(url_for("pre_game6")), gamename="Tetris")
+@app.route('/about us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/rate us')
+def rate_us():
+    return render_template('rate_us.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
