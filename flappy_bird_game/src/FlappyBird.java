fda4471970fe2bde878b83a47c584634b49a35ee
Arjun <tyagiarjun1809@gmail.com>
2024-09-25 10:08:51 +0530

    setting up the flappy_project

---
+++ flappy_bird_game/src/FlappyBird.java
@@ -0,0 +1,82 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class FlappyBird extends JPanel implements ActionListener{
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    //Images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    //Bird
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    // game logic
+    Bird bird;
+    int velocityY = -6;
+    Timer gameLoop;
+
+
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth,boardHeight));
+        //setBackground(Color.blue);
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        // Bird
+        bird = new Bird(birdImg);
+
+        //game timer
+        gameLoop = new Timer(1000/60,this); //1000/60 = 16.6
+        gameLoop.start();
+
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g) {
+        System.out.println("draw");
+        //background
+        g.drawImage(backgroundImg,0,0,boardWidth,boardHeight,null);
+
+        //bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width,bird.height, null);
+    }
+    public void move() {
+        //bird
+        bird.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+}
