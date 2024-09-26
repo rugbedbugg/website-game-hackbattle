202b8a27daac54ccb930e3afae13c7852a6dc321
aritraghosh2005 <aritraghosh.ag2005@gmail.com>
2024-09-26 10:03:26 +0530

    Creating tiles on the canvas and customizing title

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -6,10 +6,15 @@ public class TicTacToe
     int boardWidth = 600;
     int boardHeight = 650;
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic Tac Toe");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
 
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
 
     TicTacToe()
     {
@@ -30,8 +35,23 @@ public class TicTacToe
 
         textPanel.setLayout(new BorderLayout());
         textPanel.add(textLabel);
-        frame.add(textPanel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 3; c++)
+            {
+                 JButton tile = new JButton();
+                 board[r][c] = tile;
+                 boardPanel.add(tile);
+            }
+        }
+
 
     }
-    
+
 }
